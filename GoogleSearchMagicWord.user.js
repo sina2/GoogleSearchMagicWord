4847bf934a160091011436535adb2e7bbdbe065b
sina <sina.moemoe@gmail.com>
2025-03-06 15:57:09 +0900

    Create GoogleSearchMagicWord.user.js

---
+++ GoogleSearchMagicWord.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         Google Search Magic Word
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.google.co.jp/search?*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    var magicWord = "&udm=14"
+
+    console.log(location.href);
+    console.log(location.href.indexOf(magicWord));
+
+    if( location.href.indexOf(magicWord) == -1){
+    // magicWordが見つからない場合
+        location.href=location.href + magicWord;
+    } ;
+
+})();
